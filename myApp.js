31e956930a8d10d8ae269d5950ac615d10ed9db2
mohitsonippfas <143504988+mohitsonippfas@users.noreply.github.com>
2023-12-13 09:13:07 +0000

    middle ware testing

--- myApp.js
+++ myApp.js
@@ -1,6 +1,12 @@
 let express = require('express');
 let app = express();
 
+
+
+// app.use(express.static(__dirname + "/public"));
+app.use("/public", express.static(__dirname + "/public"));
+
+
 app.get("/",(req,res)=>{
     res.sendFile(__dirname + '/views/index.html')
 })
