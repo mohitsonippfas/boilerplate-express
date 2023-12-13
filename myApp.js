b752c8dcf9ad7259a4adbcc87b09fd904d2308c2
mohitsonippfas <143504988+mohitsonippfas@users.noreply.github.com>
2023-12-13 10:50:13 +0000

    jk

--- myApp.js
+++ myApp.js
@@ -2,6 +2,10 @@ require('dotenv').config();
 let express = require('express');
 let app = express();
 
+const note = {
+  "message":"Hello json"
+};
+
 // midleware to set design 
 app.use("/public", express.static(__dirname + "/public"));
 
@@ -9,15 +13,27 @@ app.get("/",(req,res)=>{
     res.sendFile(__dirname + '/views/index.html')
 })
 
-app.get("/json", (req, res) => {
-  let response;
-  if (process.env.MESSAGE_STYLE == "uppercase") {
-    response = "Hello Json".toUpperCase();
-  } else {
-    response = "Hello Json";
-  }
-  res.json(process.env.MESSAGE_STYLE)
-  res.json({ "message": response });
+// app.get("/json", (req, res) => {
+//   let response;
+//   if (process.env.MESSAGE_STYLE == "uppercase") {
+//     response = "Hello Json".toUpperCase();
+//   } else {
+//     response = "Hello Json";
+//   }
+//   res.json(process.env.MESSAGE_STYLE)
+//   res.json({ "message": response });
+// });
+
+app.get('/json', (rq, rs) => {
+
+  if(process.env.MESSAGE_STYLE == 'uppercase'){
+
+    note.message = note.message.toUpperCase();
+    
+  };
+
+  rs.json(note);
+  
 });
 
 
