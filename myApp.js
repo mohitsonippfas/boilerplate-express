9de7b84fca30a8c5985dc2c2463dabe5d8a9d650
mohitsonippfas <143504988+mohitsonippfas@users.noreply.github.com>
2023-12-14 04:49:58 +0000

    Routes Params 2 Test 1

--- myApp.js
+++ myApp.js
@@ -7,28 +7,32 @@ let app = express();
 // midleware to set design 
 app.use("/public", express.static(__dirname + "/public"));
 
-app.get("/",(req,res)=>{
+app.get("/", (req, res) => {
     res.sendFile(__dirname + '/views/index.html')
 })
 
-var message= 'Hello json';
-app.get("/json", (req, res,next) => {
-    console.log(req.method,req.path,"-",req.ip.trim());
+var message = 'Hello json';
+app.get("/json", (req, res, next) => {
+    console.log(req.method, req.path, "-", req.ip.trim());
     next();
 });
 
-app.get('/now',(req,res,next)=>{
-req.time=new Date().toString();
-next();
-},(req,res)=>{
-    res.json({time:req.time});
+app.get('/now', (req, res, next) => {
+    req.time = new Date().toString();
+    next();
+}, (req, res) => {
+    res.json({ time: req.time });
 })
 
-app.get('/:word/echo',(req,res)=>{
-    res.json({echo:req.params.word});
+app.get('/:word/echo', (req, res) => {
+    res.json({ echo: req.params.word });
 })
 
 
+app.get("/name", function (req, res) {
+    const { first: firstName, last: lastName } = req.query;
+    res.json({ name: `${firstName} ${lastName}` });
+});
 
 
 
@@ -48,4 +52,5 @@ app.get('/:word/echo',(req,res)=>{
 
 
 
- module.exports = app;
+
+module.exports = app;
